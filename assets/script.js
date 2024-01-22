6d7fe61d0790117eb347cc7722b8e9de4b236740
João Paulo Peres <peresjoao244@gmail.com>
2024-01-22 19:39:36 -0300

    Atualizando o script do projeto

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,33 @@ document.addEventListener("DOMContentLoaded", function () {
   var modal = document.getElementById("myModal");
   var closeModal = document.getElementById("closeModal");
 
+  function abrirModal(img) {
+    var modalImage = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImage.src = img.querySelector(".gallery-image").src;
+  }
+
+  // Adiciona um ouvinte de clique para cada imagem
   imgs.forEach(function (img) {
     img.addEventListener("click", function () {
-      var modalImage = document.getElementById("modalImg");
-
-      modal.style.display = "block";
-
-      modalImage.src = img.querySelector(".gallery-image").src;
+      // Verifica se a largura da tela é maior que 600 pixels antes de abrir o modal
+      if (window.innerWidth > 600) {
+        abrirModal(img);
+      }
     });
   });
 
   closeModal.addEventListener("click", function () {
     modal.style.display = "none";
+  });
+
+  // Ouvinte para redimensionamento da janela
+  window.addEventListener("resize", function () {
+    // Fecha o modal se a largura da tela for menor ou igual a 600 pixels
+    if (window.innerWidth <= 600) {
+      modal.style.display = "none";
+    }
   });
 });
 
